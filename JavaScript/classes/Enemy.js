3a6855818207ebbf3d60328c482c44f892b640ba
Nick  Reichelt <nick.reichelt@telekom.de>
2023-01-19 14:13:28 +0100

    update

--- JavaScript/classes/Enemy.js
+++ JavaScript/classes/Enemy.js
@@ -2,7 +2,7 @@ class Enemy extends Sprite {
   constructor({ position = { x: 0, y: 0 } }) {
     super({
       position,
-      imageSrc: 'img/troll1_walk.png',
+      imageSrc: 'img/org2_run.png',
       frames: {
         max: 7
       }
@@ -48,7 +48,7 @@ class Enemy extends Sprite {
     const xDistance = waypoint.x - this.center.x
     const angle = Math.atan2(yDistance, xDistance)
 
-    const speed = 1
+    const speed = 7
 
     this.velocity.x = Math.cos(angle) * speed
     this.velocity.y = Math.sin(angle) * speed
